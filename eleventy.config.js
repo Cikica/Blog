dbfeb6340cb09b502749ccf102d6bff7c4891bec
Cikito Grande <>
2025-02-07 18:40:44 +0000

    home button added to posts

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,9 +2,8 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("main.css");
     
     
-    // Array filters
+    // ARRAY
     eleventyConfig.addFilter("limit", (arr, limit) => arr.slice(0, limit));
-
     eleventyConfig.addFilter("find", (arr, key, value) => {
         return arr.find((val) => val.data[key] === value)
     });
