af528926994e43c55a543cbe72d7e049f2517d32
Cikito Grande <>
2025-02-08 12:45:23 +0000

    style fixes for footer, updated spacing on home page nad other places

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,21 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("main.css");
-    
+
+    // COLLECTIONS
+    eleventyConfig.addFilter("getNextPost", (collection, orderAsString) => {
+        let order = parseInt(orderAsString)
+        let totalPosts = collection.length;
+        let isLastPost = totalPosts === order;        
+        let orderToFind = isLastPost ? 1 : order + 1;        
+        let nextPost = collection.find((post) => {
+            console.log("- Visible order:", post.data.order)
+            console.log("- Sought order:", `${orderToFind}`)
+            console.log("-");
+            return post.data.order === `${orderToFind}`;
+        });
+
+        return nextPost
+    });
     
     // ARRAY
     eleventyConfig.addFilter("limit", (arr, limit) => arr.slice(0, limit));
